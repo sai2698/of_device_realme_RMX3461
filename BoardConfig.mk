0710eb52ccc05f943c7f53083069b17abadf49a8
sai2698 <naveendevarapalli99@gmail.com>
2022-12-03 12:49:20 +0530

    changes from forked tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,7 +76,7 @@ BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 
 NEED_KERNEL_MODULE_RECOVERY := true
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
-#BOARD_MKBOOTIMG_ARGS += --dtb $(DEVICE_PATH)/prebuilt/dtb.img
+BOARD_MKBOOTIMG_ARGS += --dtb $(DEVICE_PATH)/prebuilt/dtb.img
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
 
 # Metadata
